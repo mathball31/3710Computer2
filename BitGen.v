95584356be26a0f51cde386d14bb50cdd9bddfdf
BYee7127 <byee7127@hotmail.com>
2018-11-15 09:17:51 -0700

    Added comments and inputs/outputs for VGA and BitGen

--- BitGen.v
+++ BitGen.v
@@ -1,4 +1,12 @@
 
-module BitGen ();
+/*
+	Is a combinational circuit
+*/
+module BitGen (
+	input bright,
+	input [7:0] pixelData,
+	input [9:0] hCount, vCount,
+	output reg [7:0] rgb);
+	
 
 endmodule
--- VGAControl.v
+++ VGAControl.v
@@ -1,27 +1,26 @@
 
-/*	FPGA has a 50MHz clock and a 25MHz clock - we'll start with 50MHz
+/*	FPGA has a 50MHz clock and a 25MHz clock - recommended to us 25MHz clock
 
-	SVGA 800x600 @ 60Hz refresh and 40MHz pixel clock - we may need a wrapper for this
-		Check the stopwatch project from previous semester
+	VGA 640x480 @ 60Hz refresh and 40MHz pixel clock
 	
 	HSync
 			Timing		Clocks
-		A	3.2us			128
-		B	2.2us			88
-		C	20us			800
-		D	2us			40
-		E	26.4us		1056
+		A	3.8us			
+		B	1.6us			
+		C	25.4us		
+		D	0.6us			
+		E	31.4us		
 		
 	A = sync pulse length		B = back porch		C = active video time		D = front porch		E = scanline time
 	To find the timing, multiply the timing with the pixel clock
 	
 	VSync
 			Lines			Clocks
-		A	4				4224
-		B	23				24288
-		C	600			633600
-		C	1				1056
-		F	628			663168
+		A	2				
+		B	33				
+		C	480			
+		C	10				
+		F	525			
 	
 	A = sync pulse length		B = back porch		C = active video time		D = front porch		F = total frame time
 	To find the clock cycles, multiply the lines with the total scanline time of the HSync
@@ -29,8 +28,17 @@
 	Only cares about the HSync and VSync and the falling edge of those pulses
 	This file should only be about timing - BitGen takes care of all the drawing.
 */
-module VGAcontrol ();
+module VGAcontrol (
+	input clock, clear,
+	output reg hSync, vSync, bright,
+	output reg [9:0] hCount, vCount);
 
-	// hsync, vsync, hcount, vcount, bright (?)
+	// hsync, vsync are asserted low - high rest of the time
+	
+	//	hcount, vcount are used by BitGen to keep track of where you are on the screen
+	// best if counts are the counts of the pixels on the screen
+	// hcount = (0, 639), vcount = (0, 479)
+	
+	// bright can be asserted high or low - used by BitGen to say whether or not to draw a pixel
 
 endmodule
