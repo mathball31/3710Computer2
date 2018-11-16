a8cc2a6c450b906f520e5e39ad9258f06ef391fb
BYee7127 <byee7127@hotmail.com>
2018-11-16 16:40:16 -0700

    Working on VGAControl

--- VGAControl.v
+++ VGAControl.v
@@ -1,6 +1,5 @@
 
 /*	FPGA has a 50MHz clock and a 25MHz clock - recommended to us 25MHz clock
-
 	VGA 640x480 @ 60Hz refresh and 25MHz pixel clock
 	
 	HSync
@@ -19,7 +18,7 @@
 		A	2				62.8
 		B	33				1036.2
 		C	480			15072
-		C	10				314
+		D	10				314
 		F	525			16485
 	
 	A = sync pulse length		B = back porch		C = active video time		D = front porch		F = total frame time
@@ -30,13 +29,50 @@ module VGAcontrol (
 	output reg hSync, vSync, bright,
 	output reg [9:0] hCount, vCount);
 	
-
+	parameter HVID = 640;			// pixel width 
+	parameter HPULSE = 95;			// sync pulse length
+	parameter HBACK = 60;			// back porch length
+	parameter HFRONT = 15;			// front port length
+	parameter HMAX = 785;			// max length of horizontal pulse
 	
-	// hsync, vsync are asserted low - high rest of the time
-	always@(posedge clock)
+	parameter VVID = 480;			// pixel height
+	parameter VPUSLE = 63;			// sync pulse
+	parameter VBACK = 1036;			// back porch
+	parameter VFRONT = 314;			// front porch
+	parameter VMAX = 16485;			// max length for vertical pulse
+	
+	wire hreset;
+	wire vreset;
+	
+	// hsync, vsync are asserted low - high rest of the time <- active low
+	// use nested if loops or separate always blocks
+	always@ (posedge clock)
 	begin
-		
+		if (hreset)
+		begin
+			// reset has been fired for horizontal sync
+			hCount <= 0;
+			
+			if (vreset)
+			begin
+				vCount <= 0;
+			end
+			else 
+			begin
+				vCount <= vCount + 1;
+			end
+		end
+		else
+		begin
+			// herest == 0
+			hCount <= hCount + 1;
+			vCount <= vCount;			// make sure vCount stays constant until reset has been fired
+		end
 	end
+	
+	assign hreset = (hCount == (HMAX - 1));		// MAX - 1 because we start counting from 0
+	
+	assign vreset = (vCount == (VMAX - 1));
 	
 	
 	//	hcount, vcount are used by BitGen to keep track of where you are on the screen
