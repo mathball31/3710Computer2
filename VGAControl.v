e57d38ed9d0285340437c24a6f91580765ebc7b0
BYee7127 <byee7127@hotmail.com>
2018-11-16 21:51:20 -0700

    Figuring the finer points of VGAControl

--- VGAControl.v
+++ VGAControl.v
@@ -24,11 +24,15 @@
 	A = sync pulse length		B = back porch		C = active video time		D = front porch		F = total frame time
 	To find the clock cycles, multiply the lines with the total scanline time of the HSync
 */
-module VGAcontrol (
+module VGAControl (
 	input clock, clear,
 	output reg hSync, vSync, bright,
 	output reg [9:0] hCount, vCount);
 	
+	//	hcount, vcount are used by BitGen to keep track of where you are on the screen
+	// best if counts are the counts of the pixels on the screen
+	// hcount = (0, 639), vcount = (0, 479)
+
 	parameter HVID = 640;			// pixel width 
 	parameter HPULSE = 95;			// sync pulse length
 	parameter HBACK = 60;			// back porch length
@@ -41,8 +45,8 @@ module VGAcontrol (
 	parameter VFRONT = 314;			// front porch
 	parameter VMAX = 16485;			// max length for vertical pulse
 	
-	wire hreset;
-	wire vreset;
+	wire hreset, hsyncon, hsyncoff;
+	wire vreset, vsyncon, vsyncoff;
 	
 	// hsync, vsync are asserted low - high rest of the time <- active low
 	// use nested if loops or separate always blocks
@@ -66,20 +70,24 @@ module VGAcontrol (
 		begin
 			// herest == 0
 			hCount <= hCount + 1;
-			vCount <= vCount;			// make sure vCount stays constant until reset has been fired
+			
+			// make sure vCount stays constant until horizontal reset has been fired
+			vCount <= vCount;
 		end
 	end
 	
 	assign hreset = (hCount == (HMAX - 1));		// MAX - 1 because we start counting from 0
 	
+	// tells hsync when to fires, happens after the display has been shown, and front porch happens
+	assign hsyncon = (hCount == ((HVID + HFRONT) - 1));
+	
+	// turn off hsync
+	assign hsyncoff = (hCount == (HPULSE - 1));
+	
 	assign vreset = (vCount == (VMAX - 1));
 	
-	
-	//	hcount, vcount are used by BitGen to keep track of where you are on the screen
-	// best if counts are the counts of the pixels on the screen
-	// hcount = (0, 639), vcount = (0, 479)
-	
 	// bright can be asserted high or low - used by BitGen to say whether or not to draw a pixel
-	// this is enabled when the position is the active area of the screen
-
+	// this is enabled when the position is in the active area of the screen
+//	assign bright = (
+	
 endmodule
