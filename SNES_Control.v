aa46b54688260a2c32d0b13671e5401e09b93c81
michelle-simmons <michellecsim@gmail.com>
2018-11-28 17:21:45 -0700

    trying something new and funky with SNES_Control.. unverified

--- SNES_Control.v
+++ SNES_Control.v
@@ -9,6 +9,8 @@ module SNES_Control(clk, serial_data, snes_clk, data_latch, button_data);
 	reg [19:0] temp_counter;
 	reg [3:0] button_counter;
 	reg latch_complete = 1'b0;
+	reg q = 0;
+	reg slw_clk;
 	
 	// Place holder for the number of clock ticks that corresponds to a 
 	// data latch signal being sent out every 16.67 ms (about 60 Hz)
@@ -23,25 +25,29 @@ module SNES_Control(clk, serial_data, snes_clk, data_latch, button_data);
 	parameter TWELVEu	= 20'b0000_0000_0001_0010_1100;
 
 	// SNES controller button to clock pulse assignment
-	parameter B			= 4'b0000;
-	parameter Y			= 4'b0001;
-	parameter SELECT	= 4'b0010;
-	parameter START	= 4'b0011;
-	parameter UP		= 4'b0100;
-	parameter DOWN		= 4'b0101;
-	parameter LEFT		= 4'b0110;
-	parameter RIGHT	= 4'b0111;
-	parameter A			= 4'b1000;
-	parameter X			= 4'b1001;
-	parameter L			= 4'b1010;
-	parameter R			= 4'b1011;
+	parameter B			= 4'b0000; // 0
+	parameter Y			= 4'b0001; // 1
+	parameter SELECT	= 4'b0010; // 2
+	parameter START	= 4'b0011; // 3
+	parameter UP		= 4'b0100; // 4
+	parameter DOWN		= 4'b0101; // 5
+	parameter LEFT		= 4'b0110; // 6
+	parameter RIGHT	= 4'b0111; // 7
+	parameter A			= 4'b1000; // 8
+	parameter X			= 4'b1001; // 9 
+	parameter L			= 4'b1010; // 10
+	parameter R			= 4'b1011; // 11
 	
+	always@(posedge clk)
+	begin
+		q <= ~q;
+		slw_clk <= q;
+	end
 	
 	always @(posedge clk)
-	begin		
+	begin
 		if (counter == PULSE)
 		begin
-			
 			counter 			= 20'b0;
 			temp_counter 	= counter;
 			button_counter = 4'b1111;
@@ -70,51 +76,51 @@ module SNES_Control(clk, serial_data, snes_clk, data_latch, button_data);
 				case (button_counter)
 					B:
 					begin
-						button_data[B] = ~serial_data;
+						button_data[B] 		= ~serial_data;
 					end
 					Y:
 					begin
-						button_data[Y] = ~serial_data;
+						button_data[Y] 		= ~serial_data;
 					end
 					SELECT:
 					begin
-						button_data[SELECT] = ~serial_data;
+						button_data[SELECT] 	= ~serial_data;
 					end
 					START:
 					begin 
-						button_data[START] = ~serial_data;
+						button_data[START] 	= ~serial_data;
 					end
 					UP:
 					begin
-						button_data[UP] = ~serial_data;
+						button_data[UP] 		= ~serial_data;
 					end
 					DOWN:
 					begin
-						button_data[DOWN] = ~serial_data;
+						button_data[DOWN] 	= ~serial_data;
 					end
 					LEFT:
 					begin
-						button_data[LEFT] = !serial_data;
+						button_data[LEFT] 	= !serial_data;
 					end
 					RIGHT:
 					begin
-						button_data[RIGHT] = ~serial_data;
+						button_data[RIGHT] 	= ~serial_data;
 					end
 					A:
 					begin
-						button_data[A] = ~serial_data;
+						button_data[A] 		= ~serial_data;
 					end
 					X:
 					begin
-						button_data[X] = ~serial_data;
+						button_data[X] 		= ~serial_data;
 					end
 					L:
 					begin
-						button_data[L] = ~serial_data;
+						button_data[L] 		= ~serial_data;
 					end
 					R:
 					begin
-						button_data[R] = ~serial_data;
+						button_data[R] 		= ~serial_data;
 					end
 					4'b1111:
 					begin
